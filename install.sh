94955836275d442e99fa5ea20227312f338ca0d9
Luca Piccinini <luca.piccinini@lpsystem.eu>
2024-05-28 14:10:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 #
+# Copyright © 2024 Luca Piccinini <swipon83@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# DESCRIPTION:
 # Script for installing Django in Venv on Debian System launched as systemd service with daphne asgi async python webserver
 # Reverse proxy needed : nginx for serve static files
 #
