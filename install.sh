4b5c1fa71923b356272cd1a5da7cf6337311ebe6
Luca Piccinini <luca.piccinini@lpsystem.eu>
2024-06-03 12:02:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -890,6 +890,68 @@ python3 manage.py loaddata admin_interface_theme_uswds.json
 ## Create user alias of www-data (UID 33) for editing and have bash shell to launch in debug mode
 useradd --badname -d /var/www/django -M -g 33 -N -u 33 -o -s /bin/bash django
 
+cat <<EOF > /var/www/django/.bashrc
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+HISTCONTROL=ignoreboth
+shopt -s histappend
+HISTSIZE=1000
+HISTFILESIZE=2000
+shopt -s checkwinsize
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    #PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}[\d \t] \[\033[0;32m\]\u@\h:\[\033[0;37m\]\[\033[01;34m\][\w]\[\033[00m\]\$\[\033[0m\] '
+else
+    #PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+fi
+unset color_prompt force_color_prompt
+
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+fi
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+alias rm='rm -i'
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+source ~/bin/activate
+EOF
+
 ## Set Rights to apache/nginx user
 chown -R www-data:www-data /var/www/django
 
@@ -1162,3 +1224,8 @@ nnoremap <esc>^[ <esc>^[
 EOF
 chown -R www-data:www-data .vimrc
 deactivate
+
+echo "To login in django venv do:"
+echo "cd /var/www/django && su django"
+
+cd /var/www/django && su django
