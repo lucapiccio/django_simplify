414e0098b99ddeb352c3d69d6853fd47fa003c55
Luca Piccinini <luca.piccinini@lpsystem.eu>
2024-06-04 18:23:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1018,6 +1018,8 @@ import os
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from frontend import views as frontendviews
 from api import views as apiviews
