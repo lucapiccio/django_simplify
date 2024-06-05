b602f68fbfcb82fe599aa1c6c4c4ebad4ff92f0c
Luca Piccinini <luca.piccinini@lpsystem.eu>
2024-06-05 11:12:24 +0200

    Update make-docker.sh

--- docker/make-docker.sh
+++ docker/make-docker.sh
@@ -697,15 +697,15 @@ var asyncSuccessMessageUpdate = [
     "</button>",
     "</div>",
     "<script>",
-    "\$('.alert').fadeTo(2000, 500).slideUp(500, function () {\$('.alert').slideUp(500).remove();});",
+    "$('.alert').fadeTo(2000, 500).slideUp(500, function () {$('.alert').slideUp(500).remove();});",
     "<\/script>"
 ].join("");
 
 // modal form
 function updateBookModalForm() {
-    \$(".update-book").each(function () {
-        \$(this).modalForm({
-            formURL: \$(this).data("form-url"),
+    $(".update-book").each(function () {
+        $(this).modalForm({
+            formURL: $(this).data("form-url"),
             asyncUpdate: true,
             asyncSettings: {
                 closeOnSubmit: false,
@@ -722,8 +722,8 @@ function updateBookModalForm() {
 
 // Delete book buttons - formURL is retrieved from the data of the element
 function deleteBookModalForm() {
-    \$(".delete-book").each(function () {
-        \$(this).modalForm({formURL: \$(this).data("form-url"), isDeleteForm: true});
+    $(".delete-book").each(function () {
+        $(this).modalForm({formURL: $(this).data("form-url"), isDeleteForm: true});
     });
 }
 //deleteBookModalForm();
